4f2f47c03c1aa5352310d218e6ef8548e1f6cc9b
vkozhanova <kozhanova_vera@mail.ru>
2024-11-20 18:17:43 +0500

    JP-1-ref

--- src/main/kotlin/parser.kt
+++ src/main/kotlin/parser.kt
@@ -1,11 +1,13 @@
 package org.example
+
 import org.jsoup.Jsoup
+
 fun main() {
     val url = "https://mybook.ru/author/duglas-adams/avtostopom-po-galaktike-restoran-u-konca-vselennoj/citations/"
     try {
         val doc = Jsoup.connect(url).get()
 
-        val citations = doc.select(".sc-2aegk7-2.bzpNIu")
+        val citations = doc.select("article")
         for (i in citations) {
             println(i.text())
         }
@@ -13,3 +15,7 @@ fun main() {
         println("Ошибка при получении данных: ${e.message}")
     }
 }
+
+
+
+
